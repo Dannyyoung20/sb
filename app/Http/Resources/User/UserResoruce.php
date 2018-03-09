66382f5bf4016f3f5e4a71773688f63f33dbdce4
dansky <dan4allu93@gmail.com>
2018-03-09 00:32:42 -0500

    Added a fn to check for user role

--- app/Http/Resources/User/UserResoruce.php
+++ app/Http/Resources/User/UserResoruce.php
@@ -18,7 +18,23 @@ class UserResoruce extends Resource
             'firstname' => $this->firstname,
             'lastname' => $this->lastname,
             'phone' => $this->phone,
-            'email' => $this->email
+            'email' => $this->email,
+            'role'  => $this->userRole($request->role_id)
         ];
+    }
+
+    protected function userRole(Request $request) {
+        if($this->role_id == 1)
+            { 
+                return 'User'; 
+            }
+        elseif($this->role_id == 2) 
+            { 
+                return 'Tutor';
+            }
+        else{
+            return 'Admin';
+            }
+    
     }
 }
